b5d4a83a01eba4f0f4f1b5ebe51604e620be16e6
HakiRules <ikerelcrack530@gmail.com>
2021-07-26 19:51:25 +0200

    Help command changed.

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,38 +1,33 @@
 const { prefix } = require('../../config.json');
+const fs = require('fs');
+const Discord = require('discord.js');
+const folders = fs.readdirSync('./commands');
 
 module.exports = {
     name: 'help',
 	description: 'List all of my commands or info about a specific command.',
 	execute(message, args) {
-		const data = [];
+        const embed = new Discord.MessageEmbed();
         const { commands } = message.client;
+        embed.setColor('WHITE');
         if(!args.length) {
-            data.push('Aqui tienes todos los comandos manin: ');
-            data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nPuedes enviar ${prefix}help [comando] para tener la informacion de un comando especifico!`);
-
-            return message.author.send(data, { split: true })
-                .then(() => {
-                    if (message.channel.type === 'dm') return;
-			        essage.reply('Te he enviado un MD con todos los comandos');
-                })
-                .catch(error => {
-                    console.error(`No se ha podido enviar un MD a ${message.author.tag}.\n`, error);
-                    message.reply('Che manin, tienes los MD deshabilitados? No he podido enviarte uno.');
-                });
+            embed.setTitle('Comandos');
+            embed.setDescription(`Aqui puedes ver todos los comandos, escribe ${prefix}help [comando] para tener la informacion de un comando especifico.`);
+            for (const folder of commandFolders) {
+                let commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+                let commands = [];
+                for(const file of commandFiles) {
+                    commands.push(require(`./commands/${folder}/${file}`).name);
+                }
+                embed.addField(`${folder}`, commands.join('\n'), true);
+            }
+        }else{
+            let command = commands.get(args[0].toLowerCase());
+            if(!command) return message.reply('Eso no es un comando bro.');
+            embed.setTitle(command.name);
+            embed.setDescription(command.description);
+            embed.addField('Uso:',`${prefix}${command.name} ${command.usage}`);
         }
-
-        const name = args[0].toLowerCase();
-        const command = commands.get(name);
-
-        if (!command) {
-            return message.reply('Eso no es un comando bro.');
-        }
-
-        data.push(`**Nombre:** ${command.name}`);
-        if (command.description) data.push(`**Descripcion:** ${command.description}`);
-        if (command.usage) data.push(`**Uso:** ${prefix}${command.name} ${command.usage}`);
-
-        message.channel.send(data, { split: true });
+        message.channel.send(embed);
 	},
 }
